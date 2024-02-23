e5b4b6a355b1ee112b73fc1e23f01d9d99fbd31f
cablesalty <cablesalty@gmail.com>
2024-02-23 15:23:42 +0100

    Separated UI script from HTML to a separate js file

---
+++ src/uiscript.js
@@ -0,0 +1,59 @@
+// Modules
+const fs = require('fs');
+
+// Read config.json synchronously
+const configPath = __dirname + '/config.json'; // Path to config
+const configData = fs.readFileSync(configPath, 'utf8'); // Read file
+const config = JSON.parse(configData); // Parse json
+const modifiedConfig = JSON.parse(JSON.stringify(config)); // Create duplicate (for modification)
+
+
+// Update values based on config
+document.getElementById("currentlyBoundKeyDisplay").innerHTML = "Currently bound key: " + config.panickey;
+
+
+
+// Key Identification
+let isIdentifying = false;
+
+function startKeyIdentification() {
+    if (!isIdentifying) {
+        document.getElementById("changeBindingBtn").innerHTML = "Press a button! (Click to cancel)";
+        isIdentifying = true;
+        document.addEventListener("keydown", keyDownHandler);
+    } else {
+        isIdentifying = false;
+        document.removeEventListener("keydown", keyDownHandler);
+        document.getElementById("changeBindingBtn").innerHTML = "Change key binding...";
+    }
+}
+
+function keyDownHandler(event) {
+    const keyPressedElement = document.getElementById("currentlyBoundKeyDisplay"); // Move this line here
+    const pressedKey = event.key.toUpperCase();
+    keyPressedElement.textContent = `Currently bound key: ${pressedKey}`;
+    modifiedConfig.panickey = pressedKey;
+    document.removeEventListener("keydown", keyDownHandler);
+    isIdentifying = false;
+    document.getElementById("changeBindingBtn").innerHTML = "Change key binding...";
+}
+
+
+// Resetting to default keybind
+function resetToDefaultKeybind() {
+    modifiedConfig.panickey = "F9";
+}
+
+
+// Function saving
+async function saveConfig() {
+    modifiedConfig.panicreaction = document.getElementById("pr").value;
+    modifiedConfig.muteaudio = document.getElementById("muteaudioselection").value;
+
+    fs.writeFileSync(configPath, JSON.stringify(modifiedConfig, null, 2));
+
+    document.getElementById("saveConfigBtn").innerHTML = "Saved!";
+    setTimeout(() => {
+        document.getElementById("saveConfigBtn").innerHTML = "Save config";
+    }, 2000);
+}
